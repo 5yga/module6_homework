f7589c129597e54f0e86f22709e930ea9e6cfe4b
5yga <budadorzhiev@gmail.com>
2022-12-17 22:29:56 +0800

    finish 4 exercise

--- homework_2_module_6.js
+++ homework_2_module_6.js
@@ -1,16 +1,16 @@
 let someNumber = prompt('Введите число от 0 до 1000');
 
 function primeNumber(someNum) {
-    if (someNum <= 1000 && someNum >= 0) {
-        if (someNum !== 2 || someNum !== 3){
-            return `${someNum} - является простым числом`;
-        }
-        for (let i = 2; i < (someNum ** (1/2)); i++){
-            if (someNum % i > 0){
-                return `${someNum} - является простым числом`;
+    if (someNum == 0 || someNum == 1){
+        return `${someNum} - не является простым числом`;
+    }
+    if (someNum <= 1000 && someNum > 1) {
+        for (let i = 2; i <= (someNum ** (1/2)); i++){
+            if (someNum % i === 0){
+                return `${someNum} - не является простым числом`;
             }
         }
-        return `${someNum} - не является простым числом`;
+        return `${someNum} - является простым числом`;
     } else {
         return "Данные неверны";
     }
@@ -19,3 +19,5 @@ function primeNumber(someNum) {
 let result = primeNumber(someNumber);
 
 console.log(result);
+
+
